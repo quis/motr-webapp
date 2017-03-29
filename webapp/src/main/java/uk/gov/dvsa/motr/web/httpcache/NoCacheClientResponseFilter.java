cde76c05193518b9afb587c1211c70ad48435ae3
Przemyslaw Sempruch <przemyslaws@kainos.com>
2017-03-29 19:47:22 +0100

    a

--- webapp/src/main/java/uk/gov/dvsa/motr/web/httpcache/NoCacheClientResponseFilter.java
+++ webapp/src/main/java/uk/gov/dvsa/motr/web/httpcache/NoCacheClientResponseFilter.java
@@ -7,7 +7,6 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
-@Provider
 public class NoCacheClientResponseFilter implements ContainerResponseFilter {
 
     @Override
