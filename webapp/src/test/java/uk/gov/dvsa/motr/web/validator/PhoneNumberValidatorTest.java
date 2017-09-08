9ca6ec14e11c004b9c33c39abcf690fbcdbaf264
Mathieu Hendey <m@mathieuhendey.com>
2017-09-08 12:32:55 +0100

    Add tests

--- webapp/src/test/java/uk/gov/dvsa/motr/web/validator/PhoneNumberValidatorTest.java
+++ webapp/src/test/java/uk/gov/dvsa/motr/web/validator/PhoneNumberValidatorTest.java
@@ -13,6 +13,10 @@ import static org.mockito.Mockito.when;
 
 public class PhoneNumberValidatorTest {
 
+    private static final String PHONE_NUMBER_TOO_SHORT = "0780652671";
+    private static final String PHONE_NUMBER_TOO_LONG = "078065267111";
+    private static final String PHONE_NUMBER_LANDLINE = "02890435617";
+    private static final String PHONE_NUMBER_VALID = "07809716253";
     private SubscriptionService subscriptionService;
     private PhoneNumberValidator validator;
 
@@ -39,26 +43,34 @@ public class PhoneNumberValidatorTest {
     @Test
     public void tooShortIsInvalid() {
 
-        assertFalse(validator.isValid("0780652671"));
+        assertFalse(validator.isValid(PHONE_NUMBER_TOO_SHORT));
     }
 
     @Test
     public void tooLongIsInvalid() {
 
-        assertFalse(validator.isValid("078065267111"));
+        assertFalse(validator.isValid(PHONE_NUMBER_TOO_LONG));
     }
 
     @Test
     public void landLineIsInvalid() {
 
-        assertFalse(validator.isValid("02890435617"));
+        assertFalse(validator.isValid(PHONE_NUMBER_LANDLINE));
     }
 
     @Test
     public void validPhoneNumberIsValid() {
 
-        when(subscriptionService.hasMaxTwoSubscriptionsForPhoneNumber(any())).thenReturn(true);
+        when(subscriptionService.hasMaxTwoSubscriptionsForPhoneNumber(PHONE_NUMBER_VALID)).thenReturn(true);
 
-        assertTrue(validator.isValid("07809716253"));
+        assertTrue(validator.isValid(PHONE_NUMBER_VALID));
+    }
+
+    @Test
+    public void phoneNumberWithTwoSubscriptionsIsInvalid() {
+
+        when(subscriptionService.hasMaxTwoSubscriptionsForPhoneNumber(PHONE_NUMBER_VALID)).thenReturn(false);
+
+        assertFalse(validator.isValid(PHONE_NUMBER_VALID));
     }
 }
