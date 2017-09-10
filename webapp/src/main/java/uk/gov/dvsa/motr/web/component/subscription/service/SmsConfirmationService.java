2c20411a5ac3b4f6664429accbffabb68c9911aa
Kevin Carton <k.carton@kainos.com>
2017-09-10 21:21:51 +0100

    restrict sending of sms if rate limited

--- webapp/src/main/java/uk/gov/dvsa/motr/web/component/subscription/service/SmsConfirmationService.java
+++ webapp/src/main/java/uk/gov/dvsa/motr/web/component/subscription/service/SmsConfirmationService.java
@@ -40,7 +40,7 @@ public class SmsConfirmationService {
     private static final int ATTEMPTS = 0;
     private static final int RESEND_ATTEMPTS = 0;
     private static final int MAX_ATTEMPTS = 3;
-    private static final int MAX_RESEND_ATTEMPTS_BEFORE_RATE_LIMIT = 3;
+    private static final int MAX_RESEND_ATTEMPTS_BEFORE_RATE_LIMIT = 2;
     private static final int MIN_TIME_SECONDS_BETWEEN_RESENDS = 300;
 
     @Inject
@@ -55,12 +55,38 @@ public class SmsConfirmationService {
         this.urlHelper = urlHelper;
     }
 
-    public String handleSmsConfirmationCreation(String vrm, String phoneNumber, String confirmationId) {
+    public String handleSmsConfirmationCreation(String vrm, String phoneNumber, String confirmationId)
+            throws InvalidConfirmationIdException {
 
-        //if not already exists
-        Optional<SmsConfirmation> smsConfirmation = smsConfirmationRepository.findByConfirmationId(confirmationId);
-        if (!smsConfirmation.isPresent()) {
-            createSmsConfirmation(vrm, phoneNumber, generateCode(), confirmationId);
+        String confirmationCode = generateCode();
+        try {
+            Optional<SmsConfirmation> smsConfirmation = smsConfirmationRepository.findByConfirmationId(confirmationId);
+            if (!smsConfirmation.isPresent()) {
+                createSmsConfirmation(vrm, phoneNumber, confirmationCode, confirmationId);
+
+
+                SmsConfirmation confirmation = new SmsConfirmation()
+                        .setPhoneNumber(phoneNumber)
+                        .setCode(confirmationCode)
+                        .setVrm(vrm)
+                        .setConfirmationId(confirmationId)
+                        .setAttempts(ATTEMPTS)
+                        .setResendAttempts(RESEND_ATTEMPTS);
+
+                smsConfirmationRepository.save(confirmation, true);
+                notifyService.sendPhoneNumberConfirmationSms(phoneNumber, confirmationCode);
+                EventLogger.logEvent(
+                        new SmsConfirmationCreatedEvent().setPhoneNumber(phoneNumber).setConfirmationCode(confirmationCode));
+
+            } else if (smsSendingNotRestrictedByRateLimiting(vrm, phoneNumber, confirmationId)) {
+                notifyService.sendPhoneNumberConfirmationSms(phoneNumber, confirmationCode);
+                EventLogger.logEvent(
+                        new SmsConfirmationCreatedEvent().setPhoneNumber(phoneNumber).setConfirmationCode(confirmationCode));
+            }
+        } catch (Exception e) {
+            EventLogger.logErrorEvent(
+                    new SmsConfrimationCreationFailedEvent().setPhoneNumber(phoneNumber).setConfirmationCode(confirmationCode), e);
+            throw e;
         }
 
         return urlHelper.phoneConfirmationLink();
@@ -101,6 +127,22 @@ public class SmsConfirmationService {
         return Confirmation.CODE_NOT_VALID_MAX_ATTEMPTS_REACHED_RESEND_ALLOWED;
     }
 
+    public boolean smsSendingNotRestrictedByRateLimiting(String vrm, String phoneNumber, String confirmationId)
+            throws InvalidConfirmationIdException {
+
+        SmsConfirmation smsConfirmation = smsConfirmationRepository.findByConfirmationId(confirmationId)
+                .orElseThrow(() -> {
+                    EventLogger.logEvent(new InvalidSmsConfirmationIdUsedEvent().setUsedId(confirmationId).setPhoneNumber(phoneNumber));
+                    return new InvalidConfirmationIdException();
+                });
+
+        boolean maxResendsBeforeLimitingReached = smsConfirmation.getResendAttempts() >= MAX_RESEND_ATTEMPTS_BEFORE_RATE_LIMIT;
+        boolean insideTimeRestriction =
+                SECONDS.between(smsConfirmation.getLatestResendAttempt(), ZonedDateTime.now()) < MIN_TIME_SECONDS_BETWEEN_RESENDS;
+
+        return !(maxResendsBeforeLimitingReached && insideTimeRestriction);
+    }
+
     public String resendSms(String phoneNumber, String confirmationId)
             throws InvalidConfirmationIdException {
 
@@ -126,25 +168,7 @@ public class SmsConfirmationService {
      */
     private void createSmsConfirmation(String vrm, String phoneNumber, String confirmationCode, String confirmationId) {
 
-        SmsConfirmation smsConfirmation = new SmsConfirmation()
-                .setPhoneNumber(phoneNumber)
-                .setCode(confirmationCode)
-                .setVrm(vrm)
-                .setConfirmationId(confirmationId)
-                .setAttempts(ATTEMPTS)
-                .setResendAttempts(RESEND_ATTEMPTS);
 
-        try {
-
-            smsConfirmationRepository.save(smsConfirmation, true);
-            notifyService.sendPhoneNumberConfirmationSms(phoneNumber, confirmationCode);
-            EventLogger.logEvent(
-                    new SmsConfirmationCreatedEvent().setPhoneNumber(phoneNumber).setConfirmationCode(confirmationCode));
-        } catch (Exception e) {
-            EventLogger.logErrorEvent(
-                    new SmsConfrimationCreationFailedEvent().setPhoneNumber(phoneNumber).setConfirmationCode(confirmationCode), e);
-            throw e;
-        }
     }
 
     private void incrementAttempts(SmsConfirmation smsConfirmation) {
